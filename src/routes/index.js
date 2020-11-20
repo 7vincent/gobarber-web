5d6e9c9940d61d1786be4ab07ad9f18865d4f428
Vicente Albuquerque <7.vincent@gmail.com>
2020-11-20 18:38:17 -0300

    Rotas privadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-quotes */
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
@@ -11,7 +12,7 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
-      <Route path="/dashborad" component={Dashboard} />
+      <Route path="/dashboard" component={Dashboard} />
       <Route path="/profile" component={Profile} />
     </Switch>
   );
